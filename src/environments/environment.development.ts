b6cab3ecb2d4ff830e23ee211ca3054708c2b20e
Iurii <ternoboy@gmail.com>
2025-05-11 11:37:21 -0400

    Basic signin, landing comp., routes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,19 @@
 export const environment = {
+
     production: false,
+
     // supposing you have a backend where to send some requests
     backendUrl: "http://127.0.0.1:8000",
+    
     // The firebase config you retrieved from the console.
     // Note that this is NOT sensitive information
     firebaseConfig: {
-      apiKey: "<firebase-api-key>",
-      authDomain: "<my-project-id>.firebaseapp.com",
-      projectId: "<my-project-id>",
-      storageBucket: "<my-project-id>.appspot.com",
-      messagingSenderId: "<my-messaging-sender-id",
-      appId: "<my-app-id>",
+        apiKey: "AIzaSyAxbWLeDDJRIDf45xorZiMTHdtZ67XlrTA",
+        authDomain: "test-ang-auth.firebaseapp.com",
+        projectId: "test-ang-auth",
+        storageBucket: "test-ang-auth.firebasestorage.app",
+        messagingSenderId: "346480172811",
+        appId: "1:346480172811:web:891ff85cd4b79d57f00c0e",
+        measurementId: "G-RV90G4H1L1"
     },
-  };
+};
