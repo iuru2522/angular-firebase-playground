b20ca04313e1cfc856d496dce66fba9f5a433a88
Iurii <ternoboy@gmail.com>
2025-05-13 21:35:37 -0400

    env cofig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
     production: true,
-    backendUrl: "backendUrl",
-    firebaseConfig: {},
-  };
+    backendUrl: "backendUrl", 
+    firebaseConfig: {
+        apiKey: "AIzaSyAxbWLeDDJRIDf45xorZiMTHdtZ67XlrTA",
+        authDomain: "test-ang-auth.firebaseapp.com",
+        projectId: "test-ang-auth",
+        storageBucket: "test-ang-auth.appspot.com",
+        messagingSenderId: "346480172811",
+        appId: "1:346480172811:web:891ff85cd4b79d57f00c0e",
+        measurementId: "G-RV90G4H1L1"
+    },
+};
