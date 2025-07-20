9dc710abe0197af8e57d7616627f75aa70b02d5a
Iurii <ternoboy@gmail.com>
2025-07-19 21:10:48 -0400

    refactor: landing component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { UserService } from '../services/user.service';
 import { User, UserRole, USER_ROLE_LABELS, USER_ROLE_DESCRIPTIONS } from '../models';
 
@@ -10,26 +10,17 @@ import { User, UserRole, USER_ROLE_LABELS, USER_ROLE_DESCRIPTIONS } from '../mod
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, RouterModule]
 })
-export class LandingComponent implements OnInit {
-  user$: Observable<User | null>;
-  isAdmin$: Observable<boolean>;
-  isProjectManager$: Observable<boolean>;
-  isDeveloper$: Observable<boolean>;
-  isQATester$: Observable<boolean>;
-  isReporter$: Observable<boolean>;
-
-  constructor(private userService: UserService) {
-    this.user$ = this.userService.getCurrentUser();
-    this.isAdmin$ = this.userService.hasRole(UserRole.ADMIN);
-    this.isProjectManager$ = this.userService.hasRole(UserRole.PROJECT_MANAGER);
-    this.isDeveloper$ = this.userService.hasRole(UserRole.DEVELOPER);
-    this.isQATester$ = this.userService.hasRole(UserRole.QA_TESTER);
-    this.isReporter$ = this.userService.hasRole(UserRole.REPORTER);
-  }
-
-  ngOnInit(): void {}
+export class LandingComponent {
+  private readonly userService = inject(UserService);
+  readonly user = toSignal(this.userService.getCurrentUser());
+  readonly isAdmin = toSignal(this.userService.hasRole(UserRole.ADMIN));
+  readonly isProjectManager = toSignal(this.userService.hasRole(UserRole.PROJECT_MANAGER));
+  readonly isDeveloper = toSignal(this.userService.hasRole(UserRole.DEVELOPER));
+  readonly isQATester = toSignal(this.userService.hasRole(UserRole.QA_TESTER));
+  readonly isReporter = toSignal(this.userService.hasRole(UserRole.REPORTER));
 
   getRoleLabel(role: UserRole): string {
     return USER_ROLE_LABELS[role] || role;
